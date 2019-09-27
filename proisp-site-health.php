fd896e16480fba71db186a54edddabf030ee45af
Knut Sparhelll <knut@sparhell.no>
2019-09-27 18:21:09 +0200

    Adjusted tabs and some comment wording

--- proisp-site-health.php
+++ proisp-site-health.php
@@ -13,25 +13,25 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 if ( ! defined( 'ABSPATH' ) ) {
-	die( 'We\'re sorry, but you should not access this file directly.' );				// Silence is golden
+	die( 'We\'re sorry, but you should not access this file directly.' );	// Silence is golden
 }
 
-// Start section to be altered by PRO ISP:
+// Start section to be customized by PRO ISP:
 const PROISP_FORCE_USE_HTTPS_URLS = [
-	''      => 'https://www.proisp.eu/guides/force-https-domain/',	// Default language
-	'en_US' => 'https://www.proisp.eu/guides/force-https-domain/',	// May be omitted. If set, same as default
-	'nb_NO' => 'https://www.proisp.no/guider/sikre-https-domene/',	// Norwegian (bokmål)
-	'nn_NO' => 'https://www.proisp.no/guider/sikre-https-domene/',	// Norwegian (nynorsk)
+	''      => 'https://www.proisp.eu/guides/force-https-domain/',		// Default language
+	'en_US' => 'https://www.proisp.eu/guides/force-https-domain/',		// May be omitted. If set, same as default
+	'nb_NO' => 'https://www.proisp.no/guider/sikre-https-domene/',		// Norwegian (bokmål)
+	'nn_NO' => 'https://www.proisp.no/guider/sikre-https-domene/',		// Norwegian (nynorsk)
 ];
 
-const PROISP_REMOTE_GET_TIMEOUT = 19;								// Timeout (integer) in seconds used by wp_remote_get. Sane values: 5 to 120 (seconds).
+const PROISP_REMOTE_GET_TIMEOUT = 19;					// Timeout (integer) in seconds used by wp_remote_get. Sane values: 5 to 120 (seconds).
 const PROISP_TRANSIENT_NAME     = 'proisp_force_use_https_title';	// Name for the transient (cache entry). Stores the document title.
 const PROISP_TRANSIENT_EXPIRE   = 1 * MONTH_IN_SECONDS;	// (true)	// Possible integer values: DAY_IN_SECONDS, WEEK_IN_SECONDS, MONTH_IN_SECONDS, YEAR_IN_SECONDS
-// End section to be altered by PRO ISP.
+// End section to be customized by PRO ISP.
 // ----------------------------------------------------------------
 
-add_filter( 'site_status_test_result', 'proisp_site_health_https_link' );					// The filter hook
-function proisp_site_health_https_link( $site_health_check ) {								// The filter function, as refrenced in above 2nd argument
+add_filter( 'site_status_test_result', 'proisp_site_health_https_link' );			// The filter hook
+function proisp_site_health_https_link( $site_health_check ) {					// The filter function, as refrenced in above 2nd argument
 	// If the filtered test is not the `https_status` one, return the original result:
 	if ( 'https_status' !== $site_health_check['test'] ) {
 		return $site_health_check;
@@ -56,15 +56,15 @@ function proisp_site_health_https_link( $site_health_check ) {								// The fil
 				'type' => 'HEAD',															// Only retrive the HEAD section, not BODY, to save memory space
 				'timeout' => PROISP_REMOTE_GET_TIMEOUT,
 			] );																			// Retrievs the HEAD response of the guide document
-			$html     = wp_remote_retrieve_body( $response );								// Gets the HEAD html elements, assuming a present title element
+			$html     = wp_remote_retrieve_body( $response );					// Gets the HEAD html elements, assuming a present title element
 			$doc      = new DOMDocument();
 			@$doc->loadHTML( $html );
 			if ( $doc ) {
-				$nodes = $doc->getElementsByTagName( 'title' );								// Parses the html, find title element
+				$nodes = $doc->getElementsByTagName( 'title' );					// Parses the html, find title element
 				$title = $nodes && $nodes->item(0) ? $nodes->item(0)->nodeValue : false;	// Extracts the title string, if any
 			}
 			if ( $title ) {
-				set_transient( $transient_name, $title, PROISP_TRANSIENT_EXPIRE );			// Saves the result as a transient
+				set_transient( $transient_name, $title, PROISP_TRANSIENT_EXPIRE );		// Saves the result as a transient
 			}
 		}
 		$title = $title ?
